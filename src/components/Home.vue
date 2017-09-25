e736305ecd633c302e99f243c61fe2de6ff1a2cb
fengzi2016 <1820166258@qq.com>
2017-09-25 23:00:46 +0800

    'notEnd'

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,7 +8,7 @@
     </v-flex>
     <div class="carousel">
       <transition-group tag="ul" class="slide-ul" name="list">
-        <li v-for="(image,index) in urls" :key="index" v-show="index===currentIndex" @mouseenter='stop' @mouseleave='go' name='item'>
+        <li v-for="(image,index) in urls" :key="index"   @mouseenter='stop' @mouseleave='go' name='item' :class="{'currentLi':index===currentIndex,'nextLi':index===currentIndex+1}" >
           <a :href="image.link"></a>
           <img :src="image.url" :alt='image.url'>
         </li>
@@ -17,6 +17,18 @@
         <span v-for="(num,index) in urls.length" :class="{'active':index===currentIndex}" :key="index"  @mousemove="changeIndex(index)"></span>
       </div>
     </div>
+    <!-- <div class="css-carousel">
+      <transition-group tag="ul"  name="css-list" class="css-slide-ul" :class="{'stopAnima':ifStop}" >
+        <li v-for="(item,index) in urls" :key="index" @mouseenter='stopAnima' @mouseleave='goAnima' name='item-css'>
+        <a :href="item.link" :key="index" >
+        <img :src="item.url" :alt='item.url' :key="index">
+        </a>
+      </li>
+      </transition-group>
+      <div class="css-carouselNum">
+        <span v-for="(num,index) in urls.length" :class="{'active':index===currentIndex}" :key="index"  @mousemove="changeIndex(index)"></span>
+      </div> 
+    </div>  -->
   </v-layout>
 </template>
 
@@ -32,7 +44,8 @@ export default {
            {url:'http://echarts.baidu.com/echarts2/doc/asset/img/slide-05.jpg',link:'#'}
      ],
      currentIndex:0,
-     timeOut:''
+     timeOut:'',
+     ifStop:false,
    }   
  },
  methods:{
@@ -61,7 +74,13 @@ export default {
    if(this.currentIndex>this.urls.length-1){
      this.currentIndex=0;
    }
- }
+ },
+    stopAnima:function(){
+      this.ifStop=true;
+    },
+    goAnima:function(){
+      this.ifStop=false;
+    }
 
  }
  
@@ -72,18 +91,20 @@ export default {
   position: relative;
   min-width: 1280px;
   height: 400px;
-  overflow: hidden;
+  /* overflow: hidden; */
   background-color: #fff;
 }
 .slide-ul{
-  width: 100%;
+  list-style:none; /* 将默认的列表符号去掉 */
+  padding:0; /* 将默认的内边距去掉 */
+  margin:0; /* 将默认的外边距去掉 */
+  width:8000px;
   height: 100%;
-  
+  position: relative;
+  animation:change 40s linear 2s infinite;
 }
 .slide-ul li{
-    position:absolute;
-    width: 100%;
-    height: 100%;
+    display: inline;
   }
 .carouselNum{
   position: absolute;
@@ -94,6 +115,14 @@ export default {
   text-align: center;
   font-size: 0;
 }
+/* .currentLi{
+ position: relative;
+ animation: out 2s linear 2s;
+}
+.nextLi{
+  position: relative;
+  animation: in 2s linear 2s;
+} */
 span{
   display: inline-block;
   height: 6px;
@@ -104,6 +133,101 @@ span{
 }
 .active{
   background-color: white;
+}
+/* @keyframes out{
+  0%{
+  left: 0px ;
+  }
+
+  100%{
+    left:-1280px;
+  }
+}
+@keyframes in {
+  0%{
+    right:0px ;
+  }
+  100%{
+    left:-1280px;
+  }
+} */
+
+
+ @keyframes change{
+  0%{
+    left: 0px;
+  }
+  10%{
+    left: 0px;
+  }
+  20%{
+    left: -1280px;
+  }
+  30%{
+    left: -1280px;
+  }
+  40%{
+    left: -2560px;
+  }
+  50%{
+     left: -2560px;
+  }
+  60%{
+    left: -3840px;
+  }
+  70%{
+    left: -3840px;
+  }
+  80%{
+    left:-5120px;
+  }
+  90%{
+     left:-5120px;
+  }
+  100%{
+    left: 0px;
+  }
+}
+
+.css-carousel{
+  position: relative;
+  min-width: 1280px;
+  height: 400px;
+  /* overflow: hidden; */
+  background-color: #fff;
+}
+.css-slide-ul{
+ list-style:none;  
+  padding:0; 
+  margin:0;
+  width:8000px;
+  height: 100%;
+  position: relative;
+  animation:change 40s linear 2s infinite;
+}
+/* .slide-out{
+  position: relative;
+  animation:change 4s linear 4s infinite;
+} */
+.stopAnima{
+  animation-play-state: paused;
+}
+/* .slide-into{
+  list-style:none;  
+  padding:0; 
+  margin:0; 
+  width: 100%;
+  height: 100%;
+  position: relative;
+  animation:into 4s linear 4s infinite;
+} */
+.css-slide-ul li{
+  list-style:none;  
+  padding:0; 
+  margin:0; 
+  display: inline;
+  /* animation:change 4s linear 4s;
+  position: relative; */
 }
 </style>
 
